16689f97192159d7001b46515b872e4b88d5db9a
Brian Robert Cajulis <brianrobertcajulis@yahoo.com>
2019-12-14 14:15:11 -0800

    Added ability to create a post

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,21 +1,40 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Context } from '../context/BlogContext';
+import { FontAwesome } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
+    const { state } = useContext(Context);
+    
+    const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
+
     return (
         <View>
-            <Text>Show Screen</Text>
+            <Text>{blogPost.title}</Text>
+            <Text>{blogPost.content}</Text>
         </View>
     );
 };
 
 // Changes the header title in navigation
-ShowScreen.navigationOptions = ({ navigation }) => ({
-    title: navigation.getParam('title')
-})
+ShowScreen.navigationOptions = ({ navigation }) => {
+    return {
+        title: navigation.getParam('title'),
+        headerRight:(
+            <TouchableOpacity
+                onPress={() => navigation.navigate('Edit')}
+            >
+                <FontAwesome name="pencil" style={styles.editIcon} />
+            </TouchableOpacity>
+        ),
+    }
+}
 
 const styles = StyleSheet.create({
-
+    editIcon: {
+        fontSize: 30,
+        marginRight: 10,
+    },
 });
 
 export default ShowScreen;
